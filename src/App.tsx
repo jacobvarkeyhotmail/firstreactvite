e86e08f4292ce96005920775c2a04951c0364841
Varkey <jvarkey@dep.nyc.gov>
2025-07-10 11:47:23 -0400

    Commit

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React  Jacob</h1>
+      <h1>Vite + React  Jacob 7.0.4</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
